9ef0e87078b3b272993b7d262ed85c590faedec8
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-28 18:39:42 +0800

    Initial commit

---
+++ src/test/java/com/example/usercenter/SampleTest.java
@@ -0,0 +1,27 @@
+package com.example.usercenter;
+
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.example.mapper.UserMapper;
+import com.example.model.User;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.isTrue(5 == userList.size(), "");
+        userList.forEach(System.out::println);
+    }
+
+}
